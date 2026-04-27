36ba0f049bd272c5a87dc8b37ca10fd2ccf2ca65
chaeun12 <145729167+chaeun12@users.noreply.github.com>
2026-04-27 13:39:19 +0900

    fix:#25 유연한 테스트 객체 생성을 위한 생성자 추가

--- src/main/java/backend/drawrace/domain/round/dto/SubmitDrawingRequest.java
+++ src/main/java/backend/drawrace/domain/round/dto/SubmitDrawingRequest.java
@@ -3,11 +3,13 @@ package backend.drawrace.domain.round.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class SubmitDrawingRequest {
 
     @NotNull private Long participantId;
